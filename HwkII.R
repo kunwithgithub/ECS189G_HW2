8e9326086ac1823a09f3dc968373d6fc580007cc
ericXin95616 <xinbochao97@outlook.com>
2020-02-12 13:50:31 -0800

    form the dataset u.big

---
+++ HwkII.R
@@ -0,0 +1,67 @@
+ZIP_extractor<-function(ZIP){
+	zip_code <- as.integer(ZIP)
+	while(zip_code>=100){#truncating the number into two digits
+		zip_code <- zip_code/10
+	}
+	as.integer(zip_code)
+}
+
+lmFinalModel<-function(u.big.tst){
+	ZIP <- u.big.tst$ZIP #parse it from u.big.tst
+	u.big.tst$ZIP <- as.factor(sapply(ZIP,ZIP_extractor)) #reassign after factorizing the vector
+	hardwired_coef<-c() #lm outside of this function
+	p_rating<-rep(0,nrows(u.big.tst)) #create a vector of 0s
+	ans<-as.dataframe(cbind(u.big.tst$userId
+							,u.big.tst$movieId
+							,u.big.tst$age
+							,u.big.tst$gender
+							,p_rating)%*%hardwired_coef)
+	
+	require("MLmetrics") # a library that TA recommended
+	MAPE(u.big.tst$rating,ans$p_rating)	#MAPE 
+}
+
+# form the database we are gonna use for Hwk2
+ratings <- read.table('ml-100k/u.data')
+names(ratings) <- c('usernum', 'movienum', 'rating', 'transID')
+demog <- read.table('ml-100k/u.user', sep='|')
+names(demog) <- c('usernum', 'age', 'gender', 'occ', 'ZIP')
+u.big <- merge(ratings,demog,by.x=1,by.y=1)
+movies <- read.csv("ml-100k/u.item", sep="|", check.names=FALSE, header=FALSE, fill=TRUE)
+genres <- read.csv("ml-100k/u.genre", sep="|", check.names=FALSE, header=FALSE)
+movies <- movies[,c(-2:-5)]
+names(movies) <- c('movienum', as.character(genres[,1]))
+u.big <- merge(u.big, movies, by = "movienum", all.x = TRUE)
+u.big <- u.big[order(u.big$usernum, all$movienum),]
+
+
+tstRows <- function(seed=99){
+  set.seed(seed)
+  tstRows <- sample(1:100000, 5000)
+  return(tstRows)
+}
+
+# Example
+# predictor: age, gender, genre
+xin_lm1 <- function(u.big){
+  require(regtools)
+  # preprocess the dataset
+  
+  
+  # divide u.big into training set(95,000) and test set(5,000)
+  tst <- tstRows()
+  u.big.tst <- u.big[tst,]
+  u.big.trn <- u.big[-tst,]
+}
+
+xin_nmf1 <- function(u.big.trn, u.big.tst){
+  
+}
+
+
+
+
+
+
+
+
