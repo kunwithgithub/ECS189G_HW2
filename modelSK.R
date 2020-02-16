54a7a6e8bf33ed74bb2186f81b5f196c44072308
kunwithgithub <kunhuang@ucdavis.edu>
2020-02-16 13:43:39 -0800

    added lm2 function

--- modelSK.R
+++ modelSK.R
@@ -1,4 +1,4 @@
-#predictor: age, gender, ZIP
+#predictor: age, gender, ZIP, genre
 sk_lm1 <-function(u.big){
 	require(regtools)
 	newAge <- u.big$age
@@ -9,19 +9,29 @@ sk_lm1 <-function(u.big){
 	tst <- tstRows()
 	u.big.tst <- u.big[tst,]
 	u.big.trn <- u.big[-tst,]
-	u.big.tstX <- u.big.tst[,c(5,6,8)]
+	u.big.tstX <- u.big.tst[,c(5,6,8,9:29)]
 	u.big.tstY <- u.big.tst[,3] #rating
   
-	lmout<-lm(ratings~transID+age+ZIP+gender,u.big)
+	lmout<-lm(ratings~transID+age+ZIP+gender+indexToStr(u.big,c(9:29)),u.big)
 	lmout<-$coefficients
 	res<-roundToNearestInt(predict(lmout,u.big.tstX))
 	print(MAPE(res,u,big.tstY))
 }
 
-#predictor: age, gender, ZIP
+#predictor: age, gender, ZIP, transID, occ, genre
 sk_lm2 <-function(u.big){
 	require(regtools)
-
+	
+	tst <- tstRows()
+	u.big.tst <- u.big[tst,]
+	u.big.trn <- u.big[-tst,]
+	u.big.tstX <- u.big.tst[,c(4:8,9:29)]
+	u.big.tstY <- u.big.tst[,3] #rating
+  
+	lmout<-lm(ratings~age*transID+ZIP+gender*occ+indexToStr(u.big,c(9:29)),u.big)
+	lmout<-$coefficients
+	res<-roundToNearestInt(predict(lmout,u.big.tstX))
+	print(MAPE(res,u,big.tstY))
 }
 
 
