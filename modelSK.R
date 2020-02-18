bb4029bce2fd73341406e041194fd15f0603a599
kunwithgithub <kunhuang@ucdavis.edu>
2020-02-17 16:34:02 -0800

    modelSK.R

--- modelSK.R
+++ modelSK.R
@@ -1,38 +1,45 @@
+source("./HwkII.R")
+
 #predictor: age, gender, ZIP, genre
 sk_lm1 <-function(u.big){
-	require(regtools)
+	require('regtools')
 	newAge <- u.big$age
 	newAge[newAge<21]<-1
 	newAge[newAge>=21]<-0
-	u.big[,u.big$age]<-newAge
+	u.big$age<-newAge
 	
 	tst <- tstRows()
 	u.big.tst <- u.big[tst,]
 	u.big.trn <- u.big[-tst,]
-	u.big.tstX <- u.big.tst[,c(5,6,8,9:29)]
+	u.big.tstX <- u.big.tst[,c(5,6,8,9:27)]
 	u.big.tstY <- u.big.tst[,3] #rating
   
-	lmout<-lm(ratings~transID+age+ZIP+gender+indexToStr(u.big,c(9:29)),u.big)
-	lmout<-lmout$coefficients
+	lmout<-lm(indexToStr(u.big,c(5,6,8,9:27)),u.big)
 	res<-roundToNearestInt(predict(lmout,u.big.tstX))
-	print(MAPE(res,u,big.tstY))
+	print(MAPE(res,u.big.tstY))
 }
 
 #predictor: age, gender, ZIP, transID, occ, genre
 sk_lm2 <-function(u.big){
-	require(regtools)
+	require('regtools')
 	
 	tst <- tstRows()
 	u.big.tst <- u.big[tst,]
 	u.big.trn <- u.big[-tst,]
-	u.big.tstX <- u.big.tst[,c(4:8,9:29)]
+	u.big.tstX <- u.big.tst[,c(4:8,9:27)]
 	u.big.tstY <- u.big.tst[,3] #rating
   
-	lmout<-lm(ratings~age*transID+ZIP+gender*occ+indexToStr(u.big,c(9:29)),u.big)
-	lmout<-lmout$coefficients
+	lmout<-lm(indexToStr(u.big,c(4:8,9:27)),u.big)
 	res<-roundToNearestInt(predict(lmout,u.big.tstX))
-	print(MAPE(res,u,big.tstY))
+	print(MAPE(res,u.big.tstY))
 }
+
+u.big <- initialization()
+colnames(u.big)[13]<-'Children'
+colnames(u.big)[19]<-'FilmNoir'
+colnames(u.big)[24]<-'SciFi'
+sk_lm1(u.big)
+sk_lm2(u.big)
 
 
 
