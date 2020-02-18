4c93e82dac4151580a12fafb0fbd1b3a0a68b9c4
mesagoh <mesagoh@gmail.com>
2020-02-17 16:45:51 -0800

    melissa: lm + nmf

--- mel-hwk2.R
+++ mel-hwk2.R
@@ -22,13 +22,35 @@ source("./HwkII.R")
 
 u.big <- initialization()
 tst <- tstRows()
-tstSet <- u.big[tst,c(5:7,3)]
-trnSet <- u.big[-tst,c(5:7,3)]
+tstSet <- u.big[tst,]
+trnSet <- u.big[-tst,]
 
-# Predict: movie rating
-# Features: age, gender, occ
-lmout1 <- lm(rating ~ age + gender + occ, data=trnSet)
+# Predict: rating
+# Features: age x gender x usernum
+# PE: 0.92
+# Features: age x occ
+# PE: 0.85
+# Features: age x gender + usernum + movienum
+# PE: 0.87
+
+
+lmout1 <- lm(rating ~ age*gender + usernum + movienum, data=trnSet)
+summary(lmout1)
 res <- predict(lmout1, tstSet)
 res <- roundToNearestInt(res)
+errvec <- abs(res - tstSet[["rating"]])
+err <- MAPE(res, tstSet[["rating"]])
+err
+plot(errvec)
 
-err<-MAPE(res, tstSet[,ncol(tstSet)])
+
+# NMF starts here
+library(recosystem)
+r <- Reco()
+train_set <- data_memory(trnSet$usernum, trnSet$movienum, rating=trnSet$rating) 
+test_set <- data_memory(tstSet$usernum, tstSet$movienum, rating=NULL)
+r$train(train_set)
+pred <- r$predict(test_set, out_memory())
+pred <- roundToNearestInt(pred)
+err <- MAPE(pred, trnSet$rating)
+err
