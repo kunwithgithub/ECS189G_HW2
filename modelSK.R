04ed24f4b6f7681409358026afcf25ee2119dd72
kunwithgithub <kunhuang@ucdavis.edu>
2020-02-24 21:25:25 -0800

    modified sk_lm2

--- modelSK.R
+++ modelSK.R
@@ -26,10 +26,11 @@ sk_lm2 <-function(u.big){
 	tst <- tstRows()
 	u.big.tst <- u.big[tst,]
 	u.big.trn <- u.big[-tst,]
-	u.big.tstX <- u.big.tst[,c(4:8,9:27)]
+	u.big.tstX <- u.big.tst[,c(1,2,4:8,9:27)]
 	u.big.tstY <- u.big.tst[,3] #rating
   
-	lmout<-lm(paste(indexToStr(u.big,c(9:27)),"+age*transID"),u.big)
+	lmout<-lm(paste(indexToStr(u.big,c(1,2,9:27)),"+age*transID"),u.big)
+	summary(lmout)
 	res<-roundToNearestInt(predict(lmout,u.big.tstX))
 	print(MAPE(res,u.big.tstY))
 }
@@ -44,8 +45,15 @@ sk_nmf <- function(u.big) {
   trnst <- data_memory(u.big.trn$usernum, u.big.trn$movienum, u.big.trn$rating)
   tststX <- data_memory(u.big.tst$usernum, u.big.tst$movienum, NULL)
   tststY <- u.big.tst[,3]
-  opts = r$tune(trnst, opts = list(dim = c(10, 20, 30), costp_l1 = 0, costq_l1 = 0, nthread = 1, niter = 10))
-  r$train(trnst, opts = list(dim = 20, nmf = TRUE))
+  res = r$tune(
+		trnst,
+		opts = list(dim = c(10, 20, 30,40,50),
+		costp_l1 = c(0,0.1,0.2), costq_l1 = c(0,0.1,0.2),
+		lrate = c(0.05, 0.1, 0.2), nthread = 2)
+	)
+
+
+  r$train(trnst, opts = res$min)
   res <- r$predict(tststX, out_memory())
   print(MAPE(res, tststY))
 }
@@ -54,9 +62,8 @@ u.big <- initialization()
 colnames(u.big)[13]<-'Children'
 colnames(u.big)[19]<-'FilmNoir'
 colnames(u.big)[24]<-'SciFi'
-sk_lm1(u.big)
 sk_lm2(u.big)
-sk_nmf(u.big)
+
 
 
 
